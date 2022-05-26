6566f99fb18af7995db9a1cc9f87fd46275a86d1
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-05-26 16:10:40 +0530

    started with modal component

--- frontend/src/components/BeloggHeader.js
+++ frontend/src/components/BeloggHeader.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/images/logo.png'
 import HomeIcon from "@material-ui/icons/Home";
 import { Button } from '@material-ui/core';
 import FeaturedPlayListOutlinedIcon from "@material-ui/icons/FeaturedPlayListOutlined";
 import {
     AssignmentTurnedInOutlined,
+    // Close,
     // HomeIcon,
     // FeaturedPlayListOutlinedIcon,
     NotificationsOutlined,
@@ -13,8 +14,15 @@ import {
     // ExpandMore,
   } from "@material-ui/icons";
 import { Avatar } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close'
 import './css/BeloggHeader.css'
+import Modal from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css'
 function BeloggHeader() {
+  const [isModalOpen, setisModalOpen] = useState(false)
+  const Close = (
+    <CloseIcon />
+  )
   return (
     <div className='bHeader'>
         <div className='bHeader-content'>
@@ -36,7 +44,24 @@ function BeloggHeader() {
             <div className='bHeader_Rem'>
               <Avatar />
             </div>
-            <Button>Add Topic</Button>
+            <Button onClick={() => setisModalOpen(true)}>Add Topic</Button>
+            <Modal
+            open = {isModalOpen}
+            closeIcon = {Close}
+            onClose = {() => setisModalOpen(false)}
+            closeOnEsc
+            center
+            closeOnOverlayClick = {false}
+            styles={{
+              overlay:{
+                height:"auto"
+              }
+            }}
+            >
+              <div className='modal__title'>
+                  This is modal Title
+              </div>
+            </Modal>
         </div>
     </div>
   )
